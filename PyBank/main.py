b3a065d6a6d4733941e571bc122e4c37090ef101
Shoua <shoua.s.chang@gmail.com>
2019-06-13 21:14:56 -0500

    Loading CSV file in main.py file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,39 @@
-#main.py document for PyBank analysis
+# Assignment 3
+# Cohort 4
+# main.py document for PyBank analysis
+
+# Resources: budget_data.csv
+# Data columns: Date[0] and Profit/Losses[1]
+
+# INSTRUCTIONS
+# Your task is to create a Python script that analyzes the records to calculate each of the following:
+# 1. The total number of months included in the dataset
+# 2. The net total amount of "Profit/Losses" over the entire period
+# 3. The average of the changes in "Profit/Losses" over the entire period
+# 4. The greatest increase in profits (date and amount) over the entire period
+# 5. The greatest decrease in losses (date and amount) over the entire period
+
+# Importing packages for CSV file
+import os
+import csv
+
+# Loading CSV file source
+csvpath = os.path.join(".", 'Resources', 'budget_data.csv')
+
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    
+    # data = list(csvreader)
+    # row_count = len(data) #total of 87 rows
+    # print(len(data))
+    # print(csvreader)
+    
+#   csv_header = next(csvreader)
+#   print(f"CSV Header: {csv_header}")
+    
+#   for row in csvreader:
+#       print(row)
+    
+    
+ 
+ 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,30 @@
-#main.py document for PyPoll analysis
+# Assignment 3
+# Cohort 4
+# main.py document for PyPoll analysis
+
+# Resources: election_data.csv
+# Data columns: Voter ID[0], County[1], Candidate[2]
+
+# INSTRUCTIONS
+# Your task is to create a Python script that analyzes the votes and calculates each of the following:
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winer of the electin based on popular vote
+
+import os
+import csv
+
+csvpath = os.path.join(".", 'Resources', 'election_data.csv')
+
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    print(csvreader)
+    
+    
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    
+    for row in csvreader:
+        print(row)
