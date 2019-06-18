3cf0a4d0e27f1684d21f2e7aaf3224d41947d935
Shoua <shoua.s.chang@gmail.com>
2019-06-17 23:54:00 -0500

    Updated analysis for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,20 +11,69 @@
 # 2. A complete list of candidates who received votes
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
-# 5. The winer of the electin based on popular vote
+# 5. The winner of the election based on popular vote
 
+#   Importing dependencies for CSV file
 import os
 import csv
 
+#   Loading CSV file
 csvpath = os.path.join(".", 'Resources', 'election_data.csv')
 
+#   Opening and reading the CSV file
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
-    print(csvreader)
+    #print(csvreader)
     
+#   Variables needed for the tasks
+    total_votes = 0
+    can_votes = 0
+
+#   Lists needed for the tasks: a list for candidate names, a list for number of votes a candidate received,
+#   and a list for the percentage of votes a candidate received
+    candidates_list = [] 
+    candidate_votes = []
+    candidate_percent = []
     
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-    
+#   Create index for rows
+#   index = []
+
+#   Using Sniffer to read header and exclude from analysis.
+    if csv.Sniffer().has_header:
+        next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+    #for row in csvreader:
+        #print(row)
+        
+#   Analysis
     for row in csvreader:
-        print(row)
+        #1.     The total number of votes casted
+        total_votes += 1
+        
+        #2.     A complete list of candidates who received votes. Only append unique values/names.
+        if row[2] not in candidates_list:
+            candidates_list.append(row[2])
+            #print(candidates_list)
+        else: #Count the number of votes received for each unique candidate
+            index = candidates_list.index(row[2])
+            candidate_votes.append(1)
+            candidate_votes[index] += 1
+             
+    for row[1] in candidate_votes:
+         #3. The percetange of votes each candidate won.
+         percent = (row[1] / total_votes) * 100
+         percent = round(percent, 3)
+         candidate_percent.append(percent)
+         
+    #for 
+        
+#   Printing outcome
+#   print("Election Results")
+#   print("-----------------------------")
+#   print(f"Total Votes: {total_votes}")
+#   print("-----------------------------")
+#   Iterate through the list to print out details for each candidate.
+#   print(f"{candidates_list[i]": )
+#   print("---------------------------------------")
+#   print(f"Winner {popular_votes_winner})") 
+ 
