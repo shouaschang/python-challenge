48f2e1866b825254883faa7bb47e2f5904bde6bf
Shoua <shoua.s.chang@gmail.com>
2019-06-18 23:21:22 -0500

    Updated to include output.txt file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -27,7 +27,8 @@ with open(csvpath, newline='') as csvfile:
     
 #   Variables needed for the tasks
     total_votes = 0
-    can_votes = 0
+    winner = 0
+    index = 0
 
 #   Lists needed for the tasks: a list for candidate names, a list for number of votes a candidate received,
 #   and a list for the percentage of votes a candidate received
@@ -50,13 +51,15 @@ with open(csvpath, newline='') as csvfile:
         #1.     The total number of votes casted
         total_votes += 1
         
-        #2.     A complete list of candidates who received votes. Only append unique values/names.
+        #2.     A complete list of candidates who received votes. Only append unique values/names
+        #       and their votes.
         if row[2] not in candidates_list:
             candidates_list.append(row[2])
+            index = candidates_list.index(row[2])
+            candidate_votes.append(1)
             #print(candidates_list)
         else: #Count the number of votes received for each unique candidate
             index = candidates_list.index(row[2])
-            candidate_votes.append(1)
             candidate_votes[index] += 1
              
     for row[1] in candidate_votes:
@@ -65,15 +68,32 @@ with open(csvpath, newline='') as csvfile:
          percent = round(percent, 3)
          candidate_percent.append(percent)
          
-    #for 
+    #4.     Winner result
+    winner = max(candidate_votes)
+    index = candidate_votes.index(winner)
+    popular_votes_winner = candidates_list[index]
         
 #   Printing outcome
-#   print("Election Results")
-#   print("-----------------------------")
-#   print(f"Total Votes: {total_votes}")
-#   print("-----------------------------")
+print("Election Results")
+print("-----------------------------")
+print(f"Total Votes: {total_votes}")
+print("-----------------------------") 
 #   Iterate through the list to print out details for each candidate.
-#   print(f"{candidates_list[i]": )
-#   print("---------------------------------------")
-#   print(f"Winner {popular_votes_winner})") 
- 
+for i in range(len(candidates_list)):
+   print(f"{str(candidates_list[i])}: {str(candidate_percent[i])} {str(candidate_votes[i])}")
+print("---------------------------------------")
+print(f"Winner {popular_votes_winner}")
+    
+    
+#   Export to PyPoll_output.txt file
+file = open('PyPoll_output.txt', 'w')
+message1 = "Election Results"
+message2 = "-----------------------------"
+message3 = str(f"Total Votes: {total_votes}")
+message4 = "-----------------------------"
+#   Iterate through the list to print out details for each candidate.
+file.write('{}\n')
+for i in range(len(candidates_list)):
+   str(f"{str(candidates_list[i])}: {str(candidate_percent[i])} {str(candidate_votes[i])}")
+message5 = "---------------------------------------"
+message6 = str(f"Winner {popular_votes_winner}")
